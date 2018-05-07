4855b67f66b5646016a083e76033cb38f9c56540
Edmund <edmundcjj@gmail.com>
2018-05-07 10:25:53 +0800

    Iterative, recursive & memorization

--- index.js
+++ index.js
@@ -32,14 +32,11 @@ function fibonacciRecursion(n) {
 var cache = {};
 function fibonacci_memorization(number) {
 
-  if (number < 1)
-    return 0;
+  if (number < 1) return 0;
 
-  if (number <= 2)
-    return 1;
+  if (number <= 2) return 1;
 
-  if (number in cache)
-    return cache[number];
+  if (number in cache) return cache[number];
 
   var value = fibonacci_memorization(number - 1) + fibonacci_memorization(number - 2);
 
