2cc1d5ccff4e57fdcdfd8309bee671964899aee6
wenhao-yap <yap.justin111@gmail.com>
2018-05-07 11:01:22 +0800

    changed to dynamic programmin

--- fibonacci.js
+++ fibonacci.js
@@ -1,30 +1,23 @@
 class Fibonacci {
-	constructor(input,listChoice){
+	constructor(input,choice){
 		this.input = input,
 		this.output = [],
-		this.choice = listChoice
+		this.choice = choice
 		this.fibonacci = (n) => {
-			if (n === 0) return 0;
-			if (n === 1) return 1;
-			return this.fibonacci(n - 2) + this.fibonacci(n - 1);
+			for(let i=2; i<n; i++){
+				this.output.push((this.output[i-2])+(this.output[i-1]));
+			}
 		}
 	}
 
 	generate(){
-		if (this.choice){
-			for(let i=0; i<this.input; i++){
-				this.output.push(this.fibonacci(i))
-			}
-			console.log("list: " + this.output);
-		}
-		console.log(this.input + "-th: " + this.fibonacci(this.input-1));
+		if(this.input >= 0) this.output.push(0);
+		if(this.input >= 1) this.output.push(1);
+		if(this.input >= 2)this.fibonacci(this.input);
+		console.log("last number: " + this.output[this.output.length-1]);
+		if (this.choice) console.log("list: " + this.output);
 	}
 }
 
-if (process.argv[1] && process.argv[2] && process.argv[3]){
-	let newFib = new Fibonacci(process.argv[2],process.argv[3]);
-	newFib.generate();
-}
-else{
-	console.log("HINT: process.argv[0]:node; process.argv[1]:fibonacci.js; process.argv[2]:n-th fibonacii number; process.argv[3]:true or false to generate list of numbers")
-}
+let newFib = new Fibonacci(process.argv[2],process.argv[3]);
+newFib.generate();
