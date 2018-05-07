c90068c429c0cebcded9401db84ac62e0b031b39
RequiemDK <kok.chee.kean@outlook.com>
2018-05-07 10:43:39 +0800

    Tail recursion

--- solution.js
+++ solution.js
@@ -7,7 +7,7 @@ function fibonacci(n) {
     return parseInt(((1 + Math.sqrt(5))**n - (1 - Math.sqrt(5))**n) / (Math.sqrt(5) * 2**n));
 };
 
-console.log(fibonacci(process.argv[2]));
+// console.log(fibonacci(process.argv[2]));
 
 function fibonacci_dynamic(n) {
     const data = [1, 1];
@@ -22,4 +22,18 @@ function fibonacci_dynamic(n) {
     return data;
 }
 
-console.log(fibonacci_dynamic(process.argv[2]));
+// console.log(fibonacci_dynamic(process.argv[2]));
+
+function fibonacci_tail_actual(i, j, n) {
+    if (n <= 2) {
+        return j;
+    } else {
+        return fibonacci_tail_actual(j, i+j, n-1);
+    };
+};
+
+function fibonacci_tail_recursive(n) {
+    return fibonacci_tail_actual(1, 1, n);
+};
+
+console.log(fibonacci_tail_recursive(process.argv[2]));
